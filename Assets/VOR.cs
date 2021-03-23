6d28dee707aa80adae140df8a99993932439e756
laguilarg99 <laguilarg1644@gmail.com>
2021-03-23 21:23:58 +0100

    VOR empezado

---
+++ Assets/VOR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VOR : MonoBehaviour
+{
+    
+    private Vector3 up;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private float valor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        valor = 0.2f;
+        up = Camera.main.transform.eulerAngles;
+        Vector3 posicionCalculada = new Vector3(valor*up.y, 5.0f + (valor*up.x), 4.0f);
+        transform.position = posicionCalculada;
+    }
+}

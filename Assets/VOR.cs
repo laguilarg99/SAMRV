e34e7ee5955149847b8154af999a5003dd7a9826
laguilarg99 <laguilarg1644@gmail.com>
2021-03-24 21:26:57 +0100

    VOR terminado

--- Assets/VOR.cs
+++ Assets/VOR.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class VOR : MonoBehaviour
 {
     
-    private Vector3 up;
+    private Quaternion up;
+    private GameObject mainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = GameObject.Find("CameraRig");
     }
 
     private float valor;
+    private int direccion = 1;
+    private int contador = 0;
 
     // Update is called once per frame
     void Update()
-    {
-        valor = 0.2f;
-        up = Camera.main.transform.eulerAngles;
-        Vector3 posicionCalculada = new Vector3(valor*up.y, 5.0f + (valor*up.x), 4.0f);
-        transform.position = posicionCalculada;
+    {   
+        valor = 2.0f;
+        up =   mainCamera.transform.GetChild(0).transform.rotation;;
+        if(up.x >= -0.075 && up.x <= 0.075 && up.y >= -0.075 && up.y <= 0.075 && contador == 1){
+            direccion = direccion * (-1);
+            contador = 0;
+            transform.position = new Vector3(0.0f, 5.0f, 4.0f);
+        }else if(up.x <= -0.075 || up.x >= 0.075 || up.y <= -0.075 || up.y >= 0.075) { 
+            contador = 1;
+            Vector3 posicionCalculada = new Vector3( (direccion)*-valor*up.y, 5.0f + (valor*up.x)*(direccion), 4.0f);
+            transform.position = posicionCalculada;
+        }
+
+        
     }
 }
